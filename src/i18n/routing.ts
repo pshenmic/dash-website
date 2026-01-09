dc98f59bfaf0ff26478eedefc22dd40cde95cbfa
Litvinov Y. <litury@icloud.com>
2026-01-09 18:01:28 +0500

    feat(i18n): add navigation helpers and locale prefix config

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,5 +2,6 @@ import { defineRouting } from 'next-intl/routing'
 
 export const routing = defineRouting({
   locales: ['en', 'ru'],
-  defaultLocale: 'en'
+  defaultLocale: 'en',
+  localePrefix: 'as-needed'
 })

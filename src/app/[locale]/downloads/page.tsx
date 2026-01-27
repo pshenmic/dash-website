43d6785ce79b2dbf474831223da25e60ab6a5104
Litvinov Y. <litury@icloud.com>
2026-01-27 18:02:47 +0500

    feat(downloads): integrate wallet sections

--- src/app/[locale]/downloads/page.tsx
+++ src/app/[locale]/downloads/page.tsx
@@ -2,6 +2,8 @@ import { setRequestLocale } from 'next-intl/server'
 import { routing } from '@/i18n/routing'
 import { DownloadsHero } from '../../_components/DownloadsHero'
 import { WalletShowcase } from '../../_components/WalletShowcase'
+import { DesktopWallets } from '../../_components/DesktopWallets'
+import { HardwareWallets } from '../../_components/HardwareWallets'
 
 export function generateStaticParams (): Array<{ locale: string }> {
   return routing.locales.map((locale) => ({ locale }))
@@ -21,6 +23,14 @@ export default async function DownloadsPage ({
 
       <section className='bg-primary-white py-12 dark:bg-primary-dark lg:py-16'>
         <WalletShowcase />
+      </section>
+
+      <section className='bg-primary-white py-12 dark:bg-primary-dark lg:py-16'>
+        <DesktopWallets />
+      </section>
+
+      <section className='bg-primary-white py-12 dark:bg-primary-dark lg:py-16'>
+        <HardwareWallets />
       </section>
     </main>
   )

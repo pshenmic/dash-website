b936b35aa45a31cf86e201da15845979817f1945
Litvinov Y. <litury@icloud.com>
2026-02-17 17:41:24 +0500

    refactor(get-started): move components to locale path

--- src/app/[locale]/get-started/page.tsx
+++ src/app/[locale]/get-started/page.tsx
@@ -1,11 +1,11 @@
 import { setRequestLocale } from 'next-intl/server'
 import { routing } from '@/i18n/routing'
-import { GetStartedHero } from '../../_components/GetStartedHero'
-import { GetStartedActions } from '../../_components/GetStartedActions'
-import { GetDash } from '../../_components/GetDash'
-import { WalletShowcase } from '../../_components/WalletShowcase'
-import { BuyDashOnline } from '../../_components/BuyDashOnline'
-import { GetGoing } from '../../_components/GetGoing'
+import { GetStartedHero } from './_components/GetStartedHero'
+import { GetStartedActions } from './_components/GetStartedActions'
+import { GetDash } from './_components/GetDash'
+import { WalletShowcase } from '@/app/_components/WalletShowcase'
+import { BuyDashOnline } from './_components/BuyDashOnline'
+import { GetGoing } from './_components/GetGoing'
 
 export function generateStaticParams (): Array<{ locale: string }> {
   return routing.locales.map((locale) => ({ locale }))

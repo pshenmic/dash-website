741d41ab3488a5be004afde70d93dcbf9d3b4706
Litvinov Y. <litury@icloud.com>
2026-02-28 17:49:24 +0500

    improve(wallet-setup): add subtitle

--- src/app/[locale]/get-started/transactions/_components/WalletSetupSection.tsx
+++ src/app/[locale]/get-started/transactions/_components/WalletSetupSection.tsx
@@ -11,9 +11,12 @@ export function WalletSetupSection (): React.ReactNode {
 
   return (
     <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
-      <Heading as='h2' size='xl' weight='extrabold' className='mb-8 leading-tight tracking-tight dark:text-white sm:text-3xl sm:leading-9'>
-        {t('title')}
-      </Heading>
+      <div className='mb-8 flex flex-col gap-1'>
+        <span className='text-lg font-extrabold text-primary-blue'>{t('subtitle')}</span>
+        <Heading as='h2' weight='extrabold' className='text-[32px] leading-8.5 dark:text-white'>
+          {t('title')}
+        </Heading>
+      </div>
 
       <div className='flex flex-col gap-4'>
         {walletItems.map((item, index) => (

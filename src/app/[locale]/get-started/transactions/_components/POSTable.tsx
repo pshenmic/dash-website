f3f93a69362d5ffda79db9b559551e43199741b0
Litvinov Y. <litury@icloud.com>
2026-03-03 16:51:21 +0500

    refactor(pos-sections): migrated to dash-ui-kit Heading

--- src/app/[locale]/get-started/transactions/_components/POSTable.tsx
+++ src/app/[locale]/get-started/transactions/_components/POSTable.tsx
@@ -1,4 +1,5 @@
 import { useTranslations } from 'next-intl'
+import { Heading } from 'dash-ui-kit/react'
 
 const posData = [
   { service: 'Binance Pay', coins: '40+', physicalPos: true, posApp: true, fees: 'None', ecommerce: true, invoice: 'partial', techSupport: true, fiat: 'Auto' },
@@ -46,9 +47,9 @@ export function POSTable (): React.ReactNode {
     <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
       <div className='mb-8 flex flex-col gap-1.25'>
         <span className='text-lg font-extrabold text-primary-blue'>{t('subtitle')}</span>
-        <h2 className='text-[32px] font-extrabold leading-8.5 text-primary-dark dark:text-white'>
+        <Heading as='h2' weight='extrabold' className='text-[32px] leading-8.5 dark:text-white'>
           {t('title')}
-        </h2>
+        </Heading>
       </div>
 
       <div className='overflow-hidden rounded-t-[24px] rounded-b-[12px] border border-primary-dark/15 bg-white shadow-soft dark:border-white/15 dark:bg-secondary-space-cadet dark:shadow-none'>
--- src/app/[locale]/get-started/transactions/_components/POSWebsites.tsx
+++ src/app/[locale]/get-started/transactions/_components/POSWebsites.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import { useTranslations } from 'next-intl'
+import { Heading } from 'dash-ui-kit/react'
 
 const posWebsites = [
   { name: 'Binance Pay', logo: '/images/transactions/logo-binancepay.svg', width: 47, height: 47 },
@@ -17,9 +18,9 @@ export function POSWebsites (): React.ReactNode {
     <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
       <div className='mb-8 flex flex-col gap-1.25'>
         <span className='text-lg font-extrabold text-primary-blue'>{t('subtitle')}</span>
-        <h2 className='text-[32px] font-extrabold leading-8.5 text-primary-dark dark:text-white'>
+        <Heading as='h2' weight='extrabold' className='text-[32px] leading-8.5 dark:text-white'>
           {t('title')}
-        </h2>
+        </Heading>
       </div>
 
       <div className='grid gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6'>

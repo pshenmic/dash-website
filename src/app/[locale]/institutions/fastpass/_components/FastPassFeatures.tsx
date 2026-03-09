acbcf0683f8d74c95f5c92434703a59cd806cb5b
Litvinov Y. <litury@icloud.com>
2026-03-09 17:13:43 +0500

    style(fastpass-features): update typography

--- src/app/[locale]/institutions/fastpass/_components/FastPassFeatures.tsx
+++ src/app/[locale]/institutions/fastpass/_components/FastPassFeatures.tsx
@@ -30,11 +30,11 @@ export function FastPassFeatures (): React.ReactNode {
               />
               <div className='pointer-events-none absolute inset-0 rounded-full shadow-[inset_0px_0px_12px_0px_rgba(12,28,51,0.12)]' />
             </div>
-            <div className='flex flex-col gap-4'>
-              <Heading as='h3' size='md' weight='extrabold' className='leading-tight tracking-tight text-primary-dark! dark:text-white!'>
+            <div className='flex flex-col gap-3.75'>
+              <Heading as='h3' size='lg' weight='extrabold' className='text-[32px] leading-10 tracking-[-0.96px] text-primary-dark! dark:text-white!'>
                 {t(`${feature.key}.title`)}
               </Heading>
-              <Text size='xs' weight='medium' className='leading-snug text-primary-dark/50! dark:text-white/50!'>
+              <Text size='xs' weight='medium' className='leading-3.5 tracking-[-0.36px] text-primary-dark/50! dark:text-white/50!'>
                 {t(`${feature.key}.description`)}
               </Text>
             </div>
